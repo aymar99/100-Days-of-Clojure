8742ca2e8e8cbca0f7378ad1b9e61113dfc2312b
aymar99 <lramya26031999@gmail.com>
2022-09-14 23:11:22 +0530

    Remove line

--- Day-004/permutate_even_odd.clj
+++ Day-004/permutate_even_odd.clj
@@ -2,5 +2,4 @@
 
 (defn get_permute [lst] (clojure.string/join "" (map #(get lst %)(vec (for [item (range (count lst))] (if (even? item) (+ item 1) (- item 1)))))))
 
-
 (doseq [item (map #(get_permute (clojure.string/split % #""))(for [i  (range (Integer/parseInt (read-line)))] (read-line)))] (println item))
